12c1d7d9736eba21bd74999828c7dd3c5b6aee54
Joyce <joycemasallajorge2006@gmail.com>
2025-11-20 10:22:33 -0300

    implementa método de ordenação QuickSort

--- Vetor.java
+++ Vetor.java
@@ -1,8 +1,8 @@
-public class Vetor{
-  private int [] elementos;
+public class Vetor {
+  private int[] elementos;
   private int tamanhoAtual;
 
-  public Vetor(int capacidade){
+  public Vetor(int capacidade) {
     this.elementos = new int[capacidade];
     this.tamanhoAtual = 0;
   }
@@ -10,61 +10,91 @@ public class Vetor{
   public void imprimirVetorParaTeste() {
     System.out.print("[");
     for (int i = 0; i < this.tamanhoAtual; i++) {
-        System.out.print(this.elementos[i]);
-        if (i < this.tamanhoAtual - 1) {
-            System.out.print(", ");
-        }
+      System.out.print(this.elementos[i]);
+      if (i < this.tamanhoAtual - 1) {
+        System.out.print(", ");
+      }
     }
     System.out.println("]");
-}
+  }
 
-  public void inserir(int elemento){
-    if(this.tamanhoAtual == elementos.length){
-        System.out.println("Vetor cheio");
-        return;
+  public void inserir(int elemento) {
+    if (this.tamanhoAtual == elementos.length) {
+      System.out.println("Vetor cheio");
+      return;
     }
-    this.elementos[this.tamanhoAtual] = elemento; //1 operação
-    this.tamanhoAtual++; //2 operações -> É constante O(1)
+    this.elementos[this.tamanhoAtual] = elemento; // 1 operação
+    this.tamanhoAtual++; // 2 operações -> É constante O(1)
 
   }
 
-  public boolean buscarSequencial (int elemento){
-    for(int i = 0; i < this.tamanhoAtual; i++){ // N operações -> percorre tudo O(N)
-        if(this.elementos[i] == elemento){
-            return true;
-        }
+  public boolean buscarSequencial(int elemento) {
+    for (int i = 0; i < this.tamanhoAtual; i++) { // N operações -> percorre tudo O(N)
+      if (this.elementos[i] == elemento) {
+        return true;
+      }
     }
     return false;
   }
 
-  public boolean buscarBinaria (int elemento){
+  public boolean buscarBinaria(int elemento) {
     int inicio = 0;
-    int fim = this.tamanhoAtual - 1; 
-    while(inicio <= fim){ // log N operações   -> pior caso: encontrado na última posição ou não encontrado
-        int meio = (inicio + fim) / 2; // 1 operação
-        if(this.elementos[meio] == elemento){
-            return true;
-        } else if(this.elementos[meio] < elemento){
-            inicio = meio + 1; // 1 operação
-        } else {
-            fim = meio - 1; // 1 operação
-        }
+    int fim = this.tamanhoAtual - 1;
+    while (inicio <= fim) { // log N operações -> pior caso: encontrado na última posição ou não encontrado
+      int meio = (inicio + fim) / 2; // 1 operação
+      if (this.elementos[meio] == elemento) {
+        return true;
+      } else if (this.elementos[meio] < elemento) {
+        inicio = meio + 1; // 1 operação
+      } else {
+        fim = meio - 1; // 1 operação
+      }
     }
-    
+
     return false;
   }
 
-  public void ordenarInsertSort(){ // pior caso tem que executar o for e o while N vezes -> O(N^2) ; melhor caso O(N) quando o array já está ordenado
-  int n = this.tamanhoAtual;
-    for (int i = 1; i < n; i++){ // N operações -> percorre todo o array que não está ordenado
+  public void ordenarInsertSort() { // pior caso tem que executar o for e o while N vezes -> O(N^2) ; melhor caso
+                                    // O(N) quando o array já está ordenado
+    int n = this.tamanhoAtual;
+    for (int i = 1; i < n; i++) { // N operações -> percorre todo o array que não está ordenado
       int valor = this.elementos[i]; // assume que o valor atual é o segundo elemento do array
       int indiceAnterior = i - 1; // o indice que vamos fazer a comparação - elemento anterior
-      while(indiceAnterior >= 0 && this.elementos[indiceAnterior] > valor){ // enquanto o anterior for maior que o valor, eles vão pra direita
-        this.elementos[indiceAnterior + 1] = this.elementos[indiceAnterior]; // troca de posição 
-        indiceAnterior--; //vai andando pra esquerda
+      while (indiceAnterior >= 0 && this.elementos[indiceAnterior] > valor) { // enquanto o anterior for maior que o valor, eles vão pra direita
+        this.elementos[indiceAnterior + 1] = this.elementos[indiceAnterior]; // troca de posição
+        indiceAnterior--; // vai andando pra esquerda
       }
-      this.elementos[indiceAnterior + 1] = valor; 
+      this.elementos[indiceAnterior + 1] = valor;
     }
-
   }
+
+  private void ordernarQuickSort(int inicio, int fim) { // O(n log n) no caso médio e melhor caso; O(n^2) no pior caso 
+    if (inicio < fim) {
+      int indicePivo = partir(inicio, fim);
+      ordernarQuickSort(inicio, indicePivo - 1);
+      ordernarQuickSort(indicePivo + 1, fim);
+    }
+  }
+
+  private int partir(int inicio, int fim) {
+    int pivo = this.elementos[fim]; // escolhe o último elemento como pivô
+    int i = (inicio - 1); 
+    for (int j = inicio; j < fim; j++) { // percorre o vetor
+      if (this.elementos[j] <= pivo) { // se o elemento atual for menor ou igual ao pivô
+        i++; 
+        int temp = this.elementos[i]; 
+        this.elementos[i] = this.elementos[j]; 
+        this.elementos[j] = temp;
+      }
+    }
+    int temp = this.elementos[i + 1]; 
+    this.elementos[i + 1] = this.elementos[fim];
+    this.elementos[fim] = temp;
+    return i + 1; 
+  }
+
+  public void ordernarQuickSort() {
+    //  usa o início = 0 e o fim = tamanhoAtual - 1 do vetor
+    ordernarQuickSort(0, this.tamanhoAtual - 1);
+}
 }
